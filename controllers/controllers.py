a2f1de387cf8199a595bc7e9167b66d886f83c80
Kurt Armendariz <kurt.armendariz@arcalabs.com.mx>
2024-12-18 13:11:30 -0600

    Oddo

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -3,25 +3,35 @@ from odoo import http
 import requests
 import json
 
+
 class MyModule(http.Controller):
     @http.route('/hi', auth='public')
     def index(self, **kw):
-        sale = http.request.env['res.partner'].search([('id','=',4798)], limit=1)
+        sale = http.request.env['res.partner'].search([('id', '=', 4798)], limit=1)
         return "Hello, world "
 
     @http.route('/poc', auth='public')
     def add(self, **kw):
         try:
             session_id = http.request.httprequest.cookies.get('session_id')
-            url = 'api_url_here?SessionID='+session_id
+            url = f'http://localhost:5000/agent-query?query=Buscar producto con ID 22&session_id={session_id}'
+
             response = requests.get(url)
-            response_data = response.json()
-            print()
+
+            if response.headers.get('Content-Type') == 'application/json':
+                response_data = response.json()
+            else:
+                response_data = {"response": response.text}
+
             return http.Response(
-                    json.dumps(response_data),
-                    content_type='application/json',
-                    status=200
-                )
+                json.dumps(response_data),
+                content_type='application/json',
+                status=200
+            )
 
         except Exception as e:
-            return {'error': str(e)}
+            return http.Response(
+                json.dumps({"error": str(e)}),
+                content_type='application/json',
+                status=500
+            )
